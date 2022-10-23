06324d59f06e93b03615a771920ace8bfd3bb06a
Trevor Larkin <106640268+tlarkin011@users.noreply.github.com>
2022-10-23 14:52:46 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={AddEmployee} exact />
+          <Route path="/edit/:id" component={EditEmployee} exact />
+        </Switch>
+      </div>
+    </GlobalProvider>
   );
 }
 
